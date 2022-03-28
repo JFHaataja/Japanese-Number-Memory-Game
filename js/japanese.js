214d1896ee82cd709acfb47a19751d50d3fb2822
Jon Haataja <jon.haataja@student.careeria.fi>
2022-03-28 10:42:54 +0300

    started coding

---
+++ js/japanese.js
@@ -0,0 +1,102 @@
+function returnNihongoList(){
+    return Object.values([
+        {
+            "id": "0",
+            "kanji": "一",
+            "hiragana": "いち",
+            "romaji":"Ichi",
+            "english":"One"
+        },
+        {
+            "id": "1",
+            "kanji": "二",
+            "hiragana": "に",
+            "romaji":"Ni",
+            "english":"Two"
+        },
+        {
+            "id": "2",
+            "kanji": "三",
+            "hiragana": "さん",
+            "romaji":"San",
+            "english":"Three"
+        },
+        {
+            "id": "3",
+            "kanji": "四",
+            "hiragana": "し、よん",
+            "romaji":"Shi, yon",
+            "english":"Four"
+        },
+        {
+            "id": "4",
+            "kanji": "五",
+            "hiragana": "ご",
+            "romaji":"Go",
+            "english":"Five"
+        },
+        {
+            "id": "5",
+            "kanji": "六",
+            "hiragana": "ろく",
+            "romaji":"Roku",
+            "english":"Six"
+        },
+        {
+            "id": "6",
+            "kanji": "七",
+            "hiragana": "しち、なな",
+            "romaji":"Shichi, nana",
+            "english":"Seven"
+        },
+        {
+            "id": "7",
+            "kanji": "八",
+            "hiragana": "はち",
+            "romaji":"Hachi",
+            "english":"Eight"
+        },
+        {
+            "id": "8",
+            "kanji": "九",
+            "hiragana": "く、きゅう",
+            "romaji":"Ku, kyuu",
+            "english":"Nine"
+        },
+        {
+            "id": "9",
+            "kanji": "十",
+            "hiragana": "じゅう",
+            "romaji":"Juu",
+            "english":"Ten"
+        },
+        {
+            "id": "10",
+            "kanji": "十一",
+            "hiragana": "じゅういち",
+            "romaji":"Juuichi",
+            "english":"Eleven"
+        },
+        {
+            "id": "11",
+            "kanji": "百",
+            "hiragana": "ひゃく",
+            "romaji":"Hyaku",
+            "english":"One hundred"
+        },
+        {
+            "id": "12",
+            "kanji": "千",
+            "hiragana": "せん",
+            "romaji":"Sen",
+            "english":"One thousand"
+        },
+        {
+            "id": "13",
+            "kanji": "一万",
+            "hiragana": "いちまん",
+            "romaji":"Ichiman",
+            "english":"Ten thousand"
+        }
+    ])
+}
